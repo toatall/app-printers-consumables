afe7f0db08334d5869a65484f44f0071ec90a80c
toatall <toatall.seek@gmail.com>
2024-04-27 14:33:07 +0500

    Обновление ссылок

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,3 +1,4 @@
+// общие настройки
 export const config = {
     // Настройки всплывающего сообщения
     toast: {
@@ -6,68 +7,170 @@ export const config = {
     }
 }
 
-export const urls = {
-    home: '/',
+// корневая ссылка
+const rootUrl = '/'
 
+// настройка ссылок
+export const urls = {
+    
+    // главная    
+    home: rootUrl,
+
+    /** 
+     * аутентификация
+     * @uses @\Pages\Auth\Login.vue
+     */
+    auth: {
+        login() { return `${rootUrl}login` },
+    },
+    
     // пользователи
     users: {
-        index: () => `/users`,
+        base: `${rootUrl}users`,
+
+        index() { return `${this.base}` },
+        create() { return `${this.base}/create` },
+        store() { return this.index() },
+        edit(id) { return `${this.update(id)}/edit` }, 
+        update(id) { return `${this.base}/${id}` },
+        delete(id) { return this.update(id) },
+        restore(id) { return `${this.update(id)}/restore` },
+
+        /**
+         * привязанные организации к текущему пользователю
+         * @uses @\Pages\Users\Organizations\Dialog.vue
+         */
         organizations: {
-            index: () => `/users/organizations`,
-            change: (id) => `/users/organizations/${id}`,
-        }
+            base: `${rootUrl}users/organizations`,
+
+            index() { return `${this.base}` },
+            change(id) { return `${this.base}/${id}` },
+        },
     },    
 
     // графики
     chart: {
-        base: `/chart`,
-        // добавленные и установленные расходные материалы
-        // за последние 30 дней
+        base: `${rootUrl}chart`,
+
+        /**
+         * добавленные и установленные расходные материалы
+         * за последние 30 дней
+         * @uses @\Pages\Dashboard\Chart.vue 
+         */
         last() { return `${this.base}/last` },
     },
 
-    // принтеры
+    // принтеры на рабочих местах
     printers: {
-        base: `/printers/workplace`,
+        // @uses @\Pages\Printers\...
+        base: `${rootUrl}printers/workplace`,
         
+        // базовые операции
         index() { return this.base },
         create() { return `${this.base}/create` },
         store() { return this.index() },
-        show: (id) => `/printers/workplace/${id}`,
-        edit: (id) => `/printers/workplace/${id}/edit`,
+        show(id) { return `${this.base}/${id}` },
+        edit(id) { return `${this.base}/${id}/edit` },
         update(id) { return this.show(id) },
         delete(id) { return this.show(id) },
-        list (idConsumable) { return `${this.base}/list/${idConsumable}` },
+
+        /**
+         * список принтеров, привязанных к расходному материалу idConsumable
+         * используется в выпадающем списке
+         * @uses @\Pages\Consumable\Count\Dialogs\Subtract.vue
+         */     
+        list(idConsumable) { return `${this.base}/list/${idConsumable}` },
+                
+        /**
+         * все принтеры (используется в выпадающем списке)
+         * @uses @\Pages\Consumable\InstalledDialog.vue
+         */
         all() { return `${this.base}/all` },
     },
 
     // расходные материалы
-    consumables: {        
+    consumables: {
+        // доступное количество расходных материалов
         counts: {
-            base: `/consumables/counts`,
+            base: `${rootUrl}consumables/counts`,
+                                                
+            index() { return this.base },
             
-            index() { return this.base },            
-            create: () => `/consumables/counts/create`,
-            store () { return this.base },
-            show: (id) => `/consumables/counts/${id}`,
-            add: (id) => `/consumables/counts/${id}/add`,
-            subtract: (consumable, count) => `/consumables/${consumable}/counts/${count}/installed`,
-            validate: () => `/consumables/counts/validate`,
-            checkExists: () => `/consumables/counts/check-exists`,
-            update(id) { return this.show(id) },
+            /**
+             * @uses @\Pages\Consumable\Count\Index.vue
+             */
+            create()  { return `${this.base}/create` }, 
+            
+            /**
+             * @uses @\Pages\Consumable\Count\Create.vue
+             */
+            store () { return this.base }, 
+            
+            /**
+             * @uses @\Pages\Consumable\Count\Index.vue
+             */
+            show(id) { return `${this.base}/${id}` }, 
+            
+            /**
+             * @uses @\Pages\Consumable\Count\Dialogs\Add.vue
+             */
+            update(id) { return this.show(id) }, 
+
+            /**
+             * @uses 
+             */
+            add(id) { return `${this.base}/${id}/add` },
+            
+            /**
+             * сохранение вычитаемого расходного материала
+             * @uses @\Pages\Consumable\Count\Dialogs\Add.vue
+             * @uses @\Pages\Consumable\InstalledDialog.vue
+             */
+            subtract(consumable, count) { return `/consumables/${consumable}/counts/${count}/installed` },
+
+            /**
+             * валидация формы добавления количества расходного материала
+             * @uses @\Pages\Consumable\Count\Create.vue
+             */
+            validate() { return `${this.base}/validate` },
+            /**
+             * поиск такого же расходного материала и с той же организацией
+             * для дальнейшего прибавления количества, а не создания новой записи
+             * @uses @\Pages\Consumable\Count\Create.vue
+             */
+            checkExists() { return `${this.base}/check-exists` },
+            
+            /**
+             * @uses @\Pages\Consumable\Count\Show.vue
+             */
             updateOrganizations(id) { return `${this.base}/${id}/update-organizations` },
 
+            /**
+             * Последние установленные(вычтенные) расходные материалы
+             * @uses @\Pages\Dashboard\LastOperationsInstalled.vue
+             */
             installed() { return `${this.base}/installed/last` },
+
+
             installMaster() { return `${this.base}/installed/master` },
 
+            /**
+             * список расходных материалов, привязанных к принтеру idPrinter
+             * @uses @\Pages\Consumable\InstalledDialog.vue
+             */
             listByPrinter(idPrinter) { return `${this.base}/list-by-printer/${idPrinter}` },
 
-            // 
+            /**
+             * журналы движения количества расходных материалов
+             * @uses resources\js\Pages\Consumable\Count\ShowJournal.vue
+             */
             journal: {
+                // журнал добавления расходных материалов                
                 added: {
                     index: (idConsumable, idConsumableCount) => `/consumables/${idConsumable}/counts/${idConsumableCount}/added`,
                     redo: (idConsumable, idConsumableCount, id) => `/consumables/${idConsumable}/counts/${idConsumableCount}/added/${id}`,
                 },
+                // журнал установки(вычитания) расходных материалов
                 installed: {
                     index: (idConsumable, idConsumableCount) => `/consumables/${idConsumable}/counts/${idConsumableCount}/installed`,
                     redo: (idConsumable, idConsumableCount, id) => `/consumables/${idConsumable}/counts/${idConsumableCount}/installed/${id}`,
@@ -78,16 +181,19 @@ export const urls = {
 
     // справочник
     dictionary: {
-        prefix: 'dictionary',
 
         // справочник принтеров
         printers: {
-            index: () => `/dictionary/printers`,
+            base: `${rootUrl}dictionary/printers`,
+
+            // базовые операции
+            index() { return `${this.base}` },
             create: () => `/dictionary/printers/create`,
             show: (id) => `/dictionary/printers/${id}`,
             edit: (id) =>  `/dictionary/printers/${id}/edit`,
             update: (id) => `/dictionary/printers/${id}`,
             delete: (id) => `/dictionary/printers/${id}`,
+            
             // привязанные расходные материалы к принтеру
             consumables: {
                 index: (id) => `/dictionary/printers/${id}/consumables`,
@@ -98,28 +204,65 @@ export const urls = {
 
         // справочник расходных материалов
         consumables: {
-            index: () => `/dictionary/consumables`,
+            base: `${rootUrl}dictionary/consumables`,
+
+            index() { return this.base },
+            
+            /**
+             * @uses @\Pages\Dictionary\Consumables\Form.vue
+             */
             store() { return this.index() },
-            create: () => `/dictionary/consumables/create`,
-            show: (id) => `/dictionary/consumables/${id}`,
-            edit: (id) => `/dictionary/consumables/${id}/edit`,           
+            create() { return `${this.base}/create` },
+            
+            /**
+             * @uses @\Pages\Dictionary\Consumables\Index.vue
+             */
+            show(id) { return `${this.base}/${id}` },
+
+            /**
+             * @uses @\Pages\Dictionary\Consumables\Show.vue
+             */
+            edit(id) { return `${this.base}/${id}/edit` },           
+            
+            /**
+             * @uses @\Pages\Dictionary\Consumables\Form.vue
+             */
             update(id) { return this.show(id) },
+
+            /**
+             * @uses @\Pages\Dictionary\Consumables\Show.vue
+             */
             delete(id) { return this.show(id) },
+            
             // привязанные принтеры к расходному материалу
             printers: {
-                index: (id) => `/dictionary/consumables/${id}/printers`,
-                add: (idConsumable, idPrinter) => `/dictionary/consumables/${idConsumable}/printers/${idPrinter}/add`,
-                delete: (idConsumable, idPrinter) => `/dictionary/consumables/${idConsumable}/printers/${idPrinter}`,
+                base(idConsumable) { return `/dictionary/consumables/${idConsumable}/printers` },
+                
+                index(idConsumable) { return this.base(idConsumable) },
+
+                /**
+                 * @uses @\Pages\Dictionary\Consumables\Printers\Index.vue 
+                 */
+                add(idConsumable, idPrinter) { return `${this.base(idConsumable)}/${idPrinter}/add` },
+
+                /**
+                 * @uses @\Pages\Dictionary\Consumables\Show.vue 
+                 */
+                delete(idConsumable, idPrinter) { return `${this.base(idConsumable)}/${idPrinter}` },
             },
         },
 
         // справочник организаций
-        organizations: {            
-            index: () => `/dictionary/organizations`,
+        organizations: {  
+            // @uses @\Pages\Dictionary\Organizations\...
+
+            base: `${rootUrl}dictionary/organizations`,
+            
+            index() { return this.base },
             store() { return this.index() },
-            create: () => `/dictionary/organizations/create`,
-            show: (id) => `/dictionary/organizations/${id}`,
-            edit: (id) => `/dictionary/organizations/${id}/edit`,           
+            create() { return `${this.base}/create` },
+            show(id) { return `${this.base}/${id}` },
+            edit(id) { return `${this.base}/${id}/edit` },           
             update(id) { return this.show(id) },
             delete(id) { return this.show(id) },
         },
